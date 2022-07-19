73df592e45fc9e8649353ffe0d45b9971d2b86cb
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-19 18:05:11 +1000

    review iterating arrays

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards() {
+    let retArray= [];
+    for (let i = 0; i < names.length; i++) {
+       retArray.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return retArray;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown <= 10 && countDown >=0) {
+        console.log(countDown);
+        countDown= countDown-1;
+        }
+}
